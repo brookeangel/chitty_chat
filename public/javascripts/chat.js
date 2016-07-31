5fd71416cbcdb9bdf585098930b1811d51c6a748
Brooke Angel <brooke.nicole.angel@gmail.com>
2016-07-31 12:50:17 -0700

    functioning chat with nickname changes

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -2,19 +2,22 @@ function Chat (socket) {
   this.socket = socket;
 }
 
-Chat.prototype.sendMessage = function (text, sender) {
-  this.socket.emit('message', {
-    text: text,
-    sender: sender
-  });
+Chat.prototype.sendMessage = function (data) {
+  this.socket.emit('message', data);
 };
 
-Chat.prototype.processCommand = function (input) {
+Chat.prototype.processCommand = function (input, id) {
   const command = input.match(/^\/\w+/)[0];
+  const options = input.match(/^\/\w+\s(.*)/)[1];
   switch (command) {
     case "/nick":
+      console.log('name changing!');
+      this.socket.emit('nicknameChangeRequest', {
+        name: options,
+        id: id
+      });
       break;
     default:
-
+      break;
   }
 };
--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -4,20 +4,48 @@ function ChatUI (form, chatBox, chatSocket) {
   this.form = form;
   this.chatBox = chatBox;
   this.chatSocket = chatSocket;
-  this.nickName = 'Guest';
+  this.id = null;
+  this.nickname = 'Guest';
   this.bindHandlers();
 }
 
 ChatUI.prototype.bindHandlers = function () {
-  this.form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    this.sendMessage(this.getMessage());
+  this.form.addEventListener('submit', this.handleSubmit.bind(this));
+
+  this.chatSocket.socket.on('memberJoined', (data) => {
+    this.id = data.id;
+    this.nickname = data.name;
+  });
+
+  this.chatSocket.socket.on('memberJoinedAnnouncement', (data) => {
+    this.addLine(data.message);
   });
 
   this.chatSocket.socket.on('message', (message) => {
-    console.log('message broadcast received!');
     this.addMessage(message);
   });
+
+  this.chatSocket.socket.on('nicknameChangeResult', (data) => {
+    this.addLine(data.message);
+    if (data.success === true && this.id === data.id) {
+      this.nickname = data.name;
+    }
+  });
+
+  this.chatSocket.socket.on('userLeftChat', (data) => {
+    this.addLine(`${data.name} left the chatroom.`);
+  });
+};
+
+ChatUI.prototype.handleSubmit = function (e) {
+  e.preventDefault();
+  const msg = this.getMessage();
+  if (msg.match(/^\//)) {
+    this.chatSocket.processCommand(msg, this.id);
+  } else {
+    this.sendMessage(msg);
+  }
+  this.form.querySelector('input[type=text]').value = "";
 };
 
 ChatUI.prototype.getMessage = function () {
@@ -26,11 +54,16 @@ ChatUI.prototype.getMessage = function () {
 };
 
 ChatUI.prototype.sendMessage = function (message) {
-  this.chatSocket.sendMessage(message);
-  const input = this.form.querySelector('input[type=text]');
-  input.value = "";
+  this.chatSocket.sendMessage({
+    message: message,
+    sender: this.nickname
+  });
+};
+
+ChatUI.prototype.addLine = function (line) {
+  this.chatBox.innerHTML += `<p>${line}</p>`;
 };
 
 ChatUI.prototype.addMessage = function (message) {
-  this.chatBox.innerHTML += `<p>${message.sender}: ${message.text}</p>`;
+  this.addLine(`${new Date().toTimeString()} <b>${message.sender}:</b> ${message.message}`);
 };
